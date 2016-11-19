7900fd8179e8f54c6e9c80c66bdc785669fc0935
Niccolo' Varalla <nv16c@my.fsu.edu>
2016-11-19 02:44:10 +0000

    lesson 7 third commit

--- Lesson7_Assignment/script.js
+++ Lesson7_Assignment/script.js
@@ -4,3 +4,10 @@ Author: Niccolo' Varalla
 Date: 11/18/16
 Filename: script.js
 */
+
+//displays the next element after the current target function display(event) 
+$(event.currentTarget).next().fadeIn("slow");
+//end of display
+
+//attach event listener to h3 elements to invoke display function when clicked
+$("h3").click(display);
